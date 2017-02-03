e4872a4d055b6d8db038af3a9654e938a8650512
Jonathan Smalls <jon@smalls.rocks>
2017-02-03 09:35:05 -0500

    feature: completed assignment 2.1

---
+++ chapter2/1.py
@@ -0,0 +1,4 @@
+# convert Celsius to Fahrenheit
+celsius    = eval(input('Enter a degree in Celsius: '));
+fahrenheit = (9 / 5) * celsius + 32;
+print(str(celsius) + ' Celsius is ' + str(fahrenheit) + ' Fahrenheit');
