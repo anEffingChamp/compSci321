1b62689ba12cd4fcb1b04260c77ed3ea5ac1558e
Jonathan Smalls <jon@smalls.rocks>
2017-03-01 09:44:59 -0500

    chore: moved home work assignments to chapter folders

---
+++ chapter4/39.py
@@ -0,0 +1,12 @@
+# Jonathan Smalls
+# jon@smalls.rocks
+# chapter 4.39 assignment
+# draw two circles
+import turtle;
+import math;
+firstX       = eval(input('Please input the first X coordinate: '));
+firstY       = eval(input('Please input the first Y coordinate: '));
+firstRadius  = eval(input('Please input the first radius: '));
+secondX      = eval(input('Please input the second X coordinate: '));
+secondY      = eval(input('Please input the second Y coordinate: '));
+secondRadius = eval(input('Please input the second radius: '));
